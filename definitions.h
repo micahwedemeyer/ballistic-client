808a1b299527fa91a374d128f860e2facedd466a
Micah Wedemeyer <me@micahwedemeyer.com>
2016-10-22 13:16:31 -0400

    Better overall structure

---
+++ definitions.h
@@ -0,0 +1,10 @@
+// I/O Pins
+#define SPEAKER_PIN A0
+#define NEOPIXEL_PIN A1
+#define IMPACT_PIN A4
+
+// LED Strip
+#define NEOPIXEL_COUNT 10
+#define NEOPIXEL_TYPE WS2812
+
+// Various Constants
---
+++ pixel_test.h
@@ -0,0 +1,1 @@
+void pixelTest(Adafruit_NeoPixel strip);
