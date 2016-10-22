3d3dcabfda1dd215aad605decf5ca746d5514704
Micah Wedemeyer <me@micahwedemeyer.com>
2016-10-22 14:16:40 -0400

    Play with software timer tone test

--- tone_test.cpp
+++ tone_test.cpp
@@ -1,29 +1,50 @@
 #include "Particle.h";
 #include "definitions.h";
 
-void toneTest() {
-  Particle.publish("Tone Test Play Start");
+int noteIndex;
+
+void playNote(int thisNote);
+void endPlay();
+
+void nextNote() {
+  noteIndex++;
+  if(noteIndex < 8) {
+    playNote(noteIndex);
+  } else {
+    endPlay();
+  }
+}
+
+void playNote(int thisNote) {
   // notes in the melody:
   int melody[] = {1908,2551,2551,2273,2551,0,2024,1908}; //C4,G3,G3,A3,G3,0,B3,C4
 
   // note durations: 4 = quarter note, 8 = eighth note, etc.:
   int noteDurations[] = {4,8,8,4,4,4,4,4 };
 
-  // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+  // stop the tone playing:
+  noTone(SPEAKER_PIN);
 
-    // to calculate the note duration, take one second
-    // divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000/noteDurations[thisNote];
-    tone(SPEAKER_PIN, melody[thisNote],noteDuration);
+  // to calculate the note duration, take one second
+  // divided by the note type.
+  //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+  int noteDuration = 1000/noteDurations[thisNote];
+  tone(SPEAKER_PIN, melody[thisNote],noteDuration);
 
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    // stop the tone playing:
-    noTone(SPEAKER_PIN);
-  }
+  // to distinguish the notes, set a minimum time between them.
+  // the note's duration + 30% seems to work well:
+  int pauseBetweenNotes = noteDuration * 1.30;
+  Timer t(pauseBetweenNotes, nextNote, true);
+  t.start();
+}
+
+void endPlay() {
   Particle.publish("Tone Test Play End");
+}
+
+void toneTest() {
+  Particle.publish("Tone Test Play Start");
+
+  noteIndex = 0;
+  playNote(noteIndex);
 }
